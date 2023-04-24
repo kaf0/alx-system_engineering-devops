84843bacc9dbd42c9b2f019af2e71df30fff81cf
“kaf0” <gertrudeseworh@gmail.com>
2023-04-24 08:11:18 -0700

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import sys
-import requests
+"""
+place holder
+"""
 
-def get_employee_todo_progress(employee_id):
-    # Fetching employee information
-    employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    employee = employee_response.json()
-
-    # Fetching employee's TODO list
-    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todos = todos_response.json()
-
-    # Calculating TODO list progress
-    completed_tasks = [todo for todo in todos if todo['completed']]
-    total_tasks = len(todos)
-
-    # Displaying TODO list progress
-    print(f"Employee {employee['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python employee_todo_progress.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        except ValueError:
-            print("Invalid employee ID. Please provide an integer as the employee ID.")
+
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
+
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
